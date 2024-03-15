98342e8cd1a0de000b7c3bbc2631cd3dc6c976c5
akbarfajjar <akbarfajjar4@gmail.com>
2024-03-15 11:42:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const display = getElementById("display");
+function appendToDisplay(input){
+    display.value  += input;
+}
+function cleardisplay(){
+    display.value ="";
+}
+function calculate(){
+try{
+    display.value = eval(display.value);
+}
+catch(error){
+    display.value = "error";
+}
+}
